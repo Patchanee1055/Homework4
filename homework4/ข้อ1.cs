d013ed3eac7e2bc6ba5fdc12f2f9e7c873d229cb
LAPTOP-TTU2QOKA\wwwpa <patchanee.spk@mail.kmutt.ac.th>
2021-04-30 22:58:33 +0700

    ข้อ1

---
+++ homework4/ข้อ1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int val = 1, i, j;
+            if (rows < 0) { Console.WriteLine("Invalid Pascal’s triangle row number."); }
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
